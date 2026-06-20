ab9b46c1f5fb915a79e9f182ea6ffcc6f145302e
bitter6749 <bitter6749@outlook.jp>
2026-06-20 16:35:29 +0900

    行列のヘルパー関数を実装

--- main.c
+++ main.c
@@ -7,6 +7,13 @@
 #define SEQ_LEN    4  // 入力する文字数(例: "cat " の四文字)
 #define EMBED_DIM  16  // 文字を表現するベクトルの次元数 (RNN の HIDDENに相当)
 
+// 行列の情報を表す構造体
+typedef struct {
+  float *data;  // ヒープ領域へのポインタ
+  int rows;     // 行数 (縦のサイズ)
+  int cols;     // 列数 (横のサイズ)
+} Matrix;
+
 typedef struct {
   float *token_embedding; // 文字をベクトルに変換する表 [VOCAB_SIZE x EMBED_DIM]
   // 【token_embedding [27 × 16] の中身のイメージ】
@@ -26,6 +33,110 @@ typedef struct {
   // 出力層の重み
   float *W_out;   // ベクトルから文字の確立に戻す行列 [VOCAB_SIZE x EMBED_DIM]
 } SimpleTransformer;
+
+// ============================
+// === 行列計算のヘルパー関数 ===
+// ============================
+
+// 行列のメモリを確保し、0で初期化する関数
+Matrix create_matrix(int rows, int cols) {
+  Matrix M;
+  M.rows = rows;
+  M.cols = cols;
+
+  // calloc: メモリの確保と初期化を同時に行う
+  M.data = (float *)calloc(rows * cols, sizeof(float));
+
+  if (M.data == NULL) {
+    fprintf(stderr, "Error: 行列のメモリの確保に失敗しました。");
+    exit(EXIT_FAILURE);
+  }
+
+  return M;
+}
+
+// 行列のメモリを安全に開放する関数
+void free_matrix(Matrix *M) {
+  if (M->data != NULL) {
+    free(M->data);
+    M->data = NULL; // 二重開放 (Double Free) を防ぐ
+  }
+
+  M->rows = 0;
+  M->cols = 0;
+}
+// 通常の行列掛け算 C = A ・ B
+void mat_mul(const Matrix *A, const Matrix *B, Matrix *C) {
+  // 行列のサイズから、計算可能かチェック
+  if (A->cols != B->rows || C->rows != A->rows || C->cols != B->cols) {
+    fprintf(stderr, "Error: 行列のサイズが不一致です。\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (int A_row = 0; A_row < A->rows; A_row++) {
+    for (int B_col = 0; B_col < B->cols; B_col++) {
+      // 各行と列の内積
+      float inner_product = 0.0f;
+
+      for (int A_col = 0; A_col < A->cols; A_col++) {
+        float a = A->data[A_row * A->cols + A_col];
+        float b = B->data[A_col * B->cols + B_col];
+
+        inner_product += a * b;
+      }
+      C->data[A_row * C->cols + B_col] = inner_product;
+    }
+  }
+}
+
+// 右側転置の行列掛け算 C = A ・ B^T
+void mat_mul_b_trans(const Matrix *A, const Matrix *B, Matrix *C) {
+  // Bは転置されるので、Aの列数とBの列数が一致すべき
+  if (A->cols != B->cols || C->rows != A-> rows || C->cols != B->rows) {
+    fprintf(stderr, "Error: 行列のサイズが不一致です。\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (int A_row = 0; A_row < A->rows; A_row++) {
+    for (int B_row = 0; B_row < B->rows; B_row++) {
+      // 各行と列の内積
+      float inner_product = 0.0f;
+
+      for (int A_col = 0; A_col < A->cols; A_col++) {
+        float a = A->data[A_row * A->cols + A_col];
+        float b = B->data[B_row * B->cols + A_col];
+        
+        inner_product += a * b;
+      }
+      C->data[A_row * C->cols + B_row] = inner_product;
+    }
+  }
+}
+
+// 左側転置の行列掛け算 C = A^T ・ B
+void mat_mul_a_trans(const Matrix *A, const Matrix *B, Matrix *C) {
+  // Aは転置されるので、Aの行数とBの行数が一致すべき
+  if (A->rows != B->rows || C->rows != A->cols || C->cols != B->cols) {
+    fprintf(stderr, "Error: 行列のサイズが不一致です。\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (int A_col; A_col < A->cols; A_col++) {
+    for (int B_col; B_col < B->cols; B_col++) {
+      // 各行の列と内積
+      float inner_product = 0.0f;
+
+      for (int A_row; A_row < A->rows; A_row++) {
+        float a = A->data[A_row * A->cols + A_col];
+        float b = B->data[A_row * B->cols + B_col];
+
+        inner_product += a * b;
+      }
+      C->data[A_col * C->cols + B_col] = inner_product;
+    }
+  }
+}
+
 
 // プロトタイプ宣言
 void forward_transformer(
@@ -291,6 +402,7 @@ void backward_attention(
       for (int d = 0; d < EMBED_DIM; d++) {
         sum += dZ[i * EMBED_DIM + d] * V[j * EMBED_DIM + d];
       }
+      dA[i * SEQ_LEN + j] = sum;
     }
   }
 
